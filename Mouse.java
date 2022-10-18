e0da7b9a6b6f12411979657ef4f693c24bb6bc25
Shreya <shreyagupta4563@gmail.com>
2022-10-18 19:03:28 +0530

    added Factorialbutton.java

--- Mouse.java
+++ Mouse.java
@@ -1,67 +1,69 @@
 
 import java.awt.*;
 import java.awt.event.*;
-class Mouse extends Frame
-{	
-	String msg="";
+
+class Mouse extends Frame {
+	String msg = "";
+
+	Mouse() {
+		addMouseListener(new MyMouseAdapter(this));
+		addWindowListener(new windowclose());
+	}
+
+	public void paint(Graphics g) {
+		g.drawString(msg, 20, 80);
+	}
+
+	public static void main(String args[]) {
+		Mouse a = new Mouse();
+		a.setSize(new Dimension(400, 400));
+		a.setTitle("Mouse Events");
+		a.setVisible(true);
+	}
+}
+
+class MyMouseAdapter extends MouseAdapter {
+	Mouse ad;
+
+	public MyMouseAdapter(Mouse ad) {
+		this.ad = ad;
+	}
+
+	public void mouseClicked(MouseEvent me) {
+		ad.msg = "Mouse Clicked";
+		ad.setBackground(Color.pink);
+		ad.setForeground(Color.black);
+		ad.setSize(400, 400);
+		ad.repaint();
+	}
+
+	public void mouseEntered(MouseEvent me) {
+		ad.msg = "Mouse Entered";
+		ad.setSize(3 * 400, 3 * 500);
+		ad.repaint();
+	}
+
 	Mouse()
 	{
 		addMouseListener(new MyMouseAdapter(this));
 		addWindowListener(new windowclose());
 	}
-	public void paint(Graphics g)
-	{
-		g.drawString(msg,20,80);
+
+	public MyMouseAdapter(Mouse ad) {
+		this.ad = ad;
 	}
-	public static void main(String args[])
-	{
-		Mouse a = new Mouse();
-		a.setSize(new Dimension (400,400));
-		a.setTitle("Mouse Events");
-		a.setVisible(true);
-	}
-}
-class MyMouseAdapter extends MouseAdapter
-{
-	Mouse ad;
-	public MyMouseAdapter(Mouse ad)
-	{
-		this.ad=ad;
-	}
-	public void mouseClicked(MouseEvent me)
-	{
-		ad.msg="Mouse Clicked";
-		ad.setBackground(Color.pink);
-		ad.setForeground(Color.black);
-		ad.setSize(400,400);
-		ad.repaint();
-	}
-	public void mouseEntered (MouseEvent me)
-	{
-		ad.msg="Mouse Entered";
-		ad.setSize(3*400,3*500);
-		ad.repaint();
-	}
-		Mouse()
-	{
-		addMouseListener(new MyMouseAdapter(this));
-		addWindowListener(new windowclose());
-	}
-	public MyMouseAdapter(Mouse ad)
-	{
-		this.ad=ad;
-	}
-	public void mouseExited(MouseEvent me)
-	{
+
+	public void mouseExited(MouseEvent me) {
 		System.exit(0);
 	}
+
 }
-public void mouseClicked(MouseEvent me)
-	{
-		ad.msg="Mouse Clicked";
+
+	public void mouseClicked(MouseEvent me) {
+		ad.msg = "Mouse Clicked";
 		ad.setBackground(Color.pink);
 		ad.setForeground(Color.black);
-		ad.setSize(400,400);
+		ad.setSize(400, 400);
 		ad.repaint();
 	}
 
@@ -72,6 +74,3 @@ class windowclose extends WindowAdapter
 		System.exit(0);
 	}
 }
-
-
-
